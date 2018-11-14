e6444a02796aafbf4966b5d7a261a880beb305b8
wwszolek <wojtusiek1@gmail.com>
2018-11-14 01:34:11 +0100

    more matrix algebra
    matrix scalar multiplication and matrix matrix multiplication

--- include/matrix.h
+++ include/matrix.h
@@ -7,14 +7,21 @@ class matrix_2d{
 		std::vector<double>mat;
 		int rows=0,cols=0;
 	public:
-		matrix_2d(int j=0,int k=0):rows(j),cols(k){
-			mat.resize(j*k);
+		matrix_2d(int j=0,int k=0,int val=0):rows(j),cols(k){
+			mat.resize(j*k,val);
 		}
 		
 		double& operator()(int,int);
 		matrix_2d T();
 		matrix_2d operator+(matrix_2d &);
 		matrix_2d operator-(matrix_2d &);
+		matrix_2d operator*(double);
+		matrix_2d operator%(matrix_2d &);
+		
+		inline int getRows();
+		inline int getCols();
 };
+
+matrix_2d operator*(double,matrix_2d &);
 
 #endif
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -8,8 +8,8 @@ double& matrix_2d::operator()(int j,int k){
 
 matrix_2d matrix_2d::T(){
 	matrix_2d Tmatrix(cols,rows);
-	for(int i=0;i<Tmatrix.rows;i++){
-		for(int j=0;j<Tmatrix.cols;j++)Tmatrix(i+1,j+1)=(*this)(j+1,i+1);
+	for(int i=1;i<=Tmatrix.rows;i++){
+		for(int j=1;j<=Tmatrix.cols;j++)Tmatrix(i,j)=(*this)(j,i);
 	}
 	return Tmatrix;
 }
@@ -17,8 +17,8 @@ matrix_2d matrix_2d::T(){
 matrix_2d matrix_2d::operator+(matrix_2d &matr){
 	if((*this).rows==matr.rows&&(*this).cols==matr.cols){
 		matrix_2d sum((*this).rows,(*this).cols);
-		for(int i=0;i<sum.rows;i++){
-			for(int j=0;j<sum.cols;j++)sum(i+1,j+1)=(*this)(i+1,j+1)+matr(i+1,j+1);
+		for(int i=1;i<=sum.rows;i++){
+			for(int j=1;j<=sum.cols;j++)sum(i,j)=(*this)(i,j)+matr(i,j);
 		}
 		return sum;
 	}else return matrix_2d();
@@ -27,9 +27,44 @@ matrix_2d matrix_2d::operator+(matrix_2d &matr){
 matrix_2d matrix_2d::operator-(matrix_2d &matr){
 	if((*this).rows==matr.rows&&(*this).cols==matr.cols){
 		matrix_2d sum((*this).rows,(*this).cols);
-		for(int i=0;i<(*this).rows;i++){
-			for(int j=0;j<(*this).cols;j++)sum(i+1,j+1)=(*this)(i+1,j+1)-matr(i+1,j+1);
+		for(int i=1;i<=(*this).rows;i++){
+			for(int j=1;j<=(*this).cols;j++)sum(i,j)=(*this)(i,j)-matr(i,j);
 		}
 		return sum;
 	}else return matrix_2d();
+}
+
+matrix_2d matrix_2d::operator*(double d){
+	matrix_2d ret(rows,cols);
+	for(int i=1;i<=ret.rows;i++){
+		for(int j=1;j<=ret.cols;j++)ret(i,j)=(*this)(i,j)*d;
+	}
+	return ret;
+}
+
+matrix_2d operator*(double d,matrix_2d &mat){
+	matrix_2d ret(mat.getRows(),mat.getCols());
+	for(int i=1;i<=ret.getRows();i++){
+		for(int j=1;j<=ret.getCols();j++)ret(i,j)=d*mat(i,j);
+	}
+	return ret;
+}
+
+matrix_2d matrix_2d::operator%(matrix_2d &mat){
+	if((*this).cols==mat.rows){
+		matrix_2d ret((*this).rows,mat.cols);
+		for(int i=1;i<=ret.rows;i++){
+			for(int j=1;j<=ret.cols;j++){
+				for(int k=1;k<=(*this).cols;k++)ret(i,j)+=(*this)(i,k)*mat(k,j);
+			}
+		}
+		return ret;
+	}else return matrix_2d();
+}
+
+inline int matrix_2d::getRows(){
+	return rows;
+}
+inline int matrix_2d::getCols(){
+	return cols;
 }
